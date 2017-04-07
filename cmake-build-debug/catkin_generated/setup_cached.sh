499c80ad3e997b54b080eadb5b45a8430471b686
carlottas <carlotta.sartore1@gmail.com>
2017-04-07 08:48:09 +0200

    changed_XYZ_RGB

--- cmake-build-debug/catkin_generated/setup_cached.sh
+++ cmake-build-debug/catkin_generated/setup_cached.sh
@@ -7,7 +7,7 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/carlotta/catkin_ws/src/pitt_geometric_tracking/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/opt/ros/indigo/lib:."
-export ROSLISP_PACKAGE_DIRECTORIES="/home/carlotta/catkin_ws/src/pitt_geometric_tracking/cmake-build-debug/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/carlotta/catkin_ws/src/pitt_geometric_tracking:$ROS_PACKAGE_PATH"
+export CMAKE_PREFIX_PATH="/home/carlotta/catkin_ws/src/PITT/pitt_geometric_tracking/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/opt/ros/indigo/lib/x86_64-linux-gnu:."
+export ROSLISP_PACKAGE_DIRECTORIES="/home/carlotta/catkin_ws/src/PITT/pitt_geometric_tracking/cmake-build-debug/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/carlotta/catkin_ws/src/pitt_geometric_tracking:/home/carlotta/catkin_ws/src/PITT/pitt_geometric_tracking:$ROS_PACKAGE_PATH"
